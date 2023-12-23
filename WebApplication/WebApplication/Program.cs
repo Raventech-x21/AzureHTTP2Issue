9bc1f6f28f0f350a51521e544377196aa751687c
Michael Copley <copley_michael@hotmail.com>
2023-12-23 16:03:09 +0800

    Added default root endpoint for backend health probe

--- WebApplication/WebApplication/Program.cs
+++ WebApplication/WebApplication/Program.cs
@@ -11,6 +11,11 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+app.MapGet("", () =>
+{
+    return "Hello";
+});
+
 app.MapGet("/weatherforecast", () =>
 {
     var forecast = Enumerable.Range(1, 5).Select(index =>
